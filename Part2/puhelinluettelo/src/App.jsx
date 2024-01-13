92821a112df0b82b213be8c67b625fad5d6b88de
Raoul Rapeli <raoul.rapeli@gmail.com>
2024-01-13 19:19:21 +0200

    Part2_6

---
+++ Part2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+const Contact = ({person}) =>{
+  return(
+    <div>
+      {person.name}
+    </div>
+  )
+}
+
+const Contacts = ({persons}) =>{
+  return (
+    <>
+      {persons.map((person, i)=> 
+        <Contact key={person+i} person={person}/>
+      )}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const chngeName = (event) => {
+    let name = event.target.value
+    setNewName(name)
+  }
+
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    let newPerson = persons.concat({name:newName})
+    setPersons(newPerson)
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={(e)=> chngeName(e)}/>
+        </div>
+        <div>
+          <button type="submit" onClick={(e)=> handleSubmit(e)}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Contacts persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
