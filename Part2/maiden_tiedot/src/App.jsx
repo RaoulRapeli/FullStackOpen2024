e155d803b578dd32ed20a8cd80d89746f766afae
Raoul Rapeli <raoul.rapeli@gmail.com>
2024-01-14 19:09:57 +0200

    Part2_18

---
+++ Part2/maiden_tiedot/src/App.jsx
@@ -0,0 +1,138 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import countryServices from "./services/callFunktions"
+
+
+const Languages = ({languages}) =>{
+
+  return (
+    <div>
+      <ul>
+        {Object.values(languages).map((language,i) => <li key={language+i}>{language}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+const CountrySpecifics = ({country}) =>{
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>capital: {country.capital[0]}</div>
+      <div>area: {country.area}</div>
+      <div>
+        <h1>languages:</h1>
+        <Languages languages={country.languages}/>
+      </div>
+      <div>
+        <img src={country.flags["png"]} alt="flag" />
+      </div>
+    </div>
+  )
+}
+ 
+
+const Country = ({country,getSpecific}) =>{
+  return(
+    <div>
+      {getSpecific!==true?
+        <>{country.name.common}</>
+        :
+        <div>
+          <CountrySpecifics {...{country}}/>
+        </div>
+      }
+    </div>
+  )
+}
+
+const Countires = ({countries}) =>{
+  return (
+    <>
+      {countries.length<=10?
+        <>
+          {countries.length>1?
+            <>
+              {countries.map((country, i)=> 
+                <Country key={country+i} country={country} getSpecific={false}/>
+              )}
+            </>
+            :
+            <>
+              {countries.map((country, i)=> 
+                <Country key={country+i} country={country} getSpecific={true}/>
+              )}
+            </>
+          }
+        </>
+        :
+        <>
+          To many matches, specify another filter
+        </>
+      }
+    </>
+  )
+}
+
+const Filter = ({newFilter,chngeFilter}) =>{
+
+  return (
+    <>
+      <input value={newFilter} onChange={chngeFilter}/>
+    </>
+  )
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const [newFilter, setNewFilter] = useState("")
+
+  useEffect(() => {
+    countryServices
+    .getAllFiltered()
+      .then(response => {
+        setCountries(response)
+      })
+  }, [])
+
+  useEffect (() => {
+    if(newFilter!==""){
+      let tempCountries = countries.filter((country) => country.name.common.toLowerCase().includes(newFilter.toLowerCase()))
+      setFilteredCountries(tempCountries)
+    }
+    else{
+      setFilteredCountries([])
+    }
+  }, [newFilter])
+
+  const chngeFilter = (event) => {
+    let filterValue = event.target.value
+    setNewFilter(filterValue)
+  }
+
+  return (
+    <div>
+      {countries.length!==0?
+        <>
+          <div>
+          find countries : <Filter newFilter={newFilter} chngeFilter={(e) => chngeFilter(e)}/>
+          </div>
+          <>
+            {filteredCountries!==undefined?
+              <Countires countries={filteredCountries}/>
+              :
+              null
+            }
+          </>
+        </>
+        :
+        null
+      }
+    </div>
+  )
+}
+
+export default App
